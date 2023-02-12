031d407b6c5d07e893262fdce4540fa43e92e4fc
daptanque <daniel.apeixoto.tanque@gmail.com>
2023-02-12 21:38:54 +0000

    All project finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Employee } from './employee';
 import { EmployeeService } from './employee.service';
 
@@ -10,6 +11,8 @@ import { EmployeeService } from './employee.service';
 })
 export class AppComponent implements OnInit{
   public employees: Employee[] = [];
+  public editEmployee!: Employee | null;
+  public deleteEmployee!: Employee | null; 
 
 
   constructor(private employeeService: EmployeeService){}
@@ -28,5 +31,96 @@ export class AppComponent implements OnInit{
       }
     );
   }
+
+  
+  public onAddEmployee(addForm: NgForm): void{
+    document.getElementById('add-employee-form')?.click();
+    //o .value entrega um json
+    this.employeeService.addEmployee(addForm.value).subscribe(
+      (response: Employee) => {
+        console.log(response);
+        this.getEmployees();
+        //clear the form
+        addForm.reset();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message)
+      },
+    );
+  }
+
+
+  public onUpdateEmployee(employee: Employee): void{
+    this.employeeService.updateEmployee(employee).subscribe(
+      (response: Employee) => {
+        console.log(response);
+        this.getEmployees();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message)
+      },
+    );
+  }
+
+
+  public onDeleteEmployee(employeeId: number): void{
+    this.employeeService.deleteEmployee(employeeId).subscribe(
+      (response: void) => {
+        console.log(response);
+        this.getEmployees();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message)
+      },
+    );
+    /**
+     * 
+     *   3.- The attributes in the components needs initialization, so employees needs to be initialized with "[ ]", editEmployee and deleteEmployee with " Employee | undefined".
+     *   4.- I needed to specify that the id of the employee to delete can't be null, so i replaced "deleteEmployee?.id" with "deleteEmployee!.id".
+     */
+  }
+
+
+  public searchEmployees(key: string): void{
+    const results: Employee[] = []
+    for(const employee of this.employees){
+      if(employee.name.toLowerCase().indexOf(key.toLowerCase())!=-1
+      || employee.email.toLowerCase().indexOf(key.toLowerCase())!=-1
+      || employee.phone.toLowerCase().indexOf(key.toLowerCase())!=-1
+      || employee.jobTitle.toLowerCase().indexOf(key.toLowerCase())!=-1){
+        results.push(employee);
+      }
+    }
+    this.employees = results;
+    if(results.length === 0 || !key){
+      this.getEmployees();
+    }
+  }
+
+  
+  public onOpenModal(employee: Employee | null, mode: string): void {
+    const container = document.getElementById('main-container');
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-toggle', 'modal');
+    if (mode === 'add') {
+      button.setAttribute('data-target', '#addEmployeeModal');
+    }
+    if (mode === 'edit') {
+      this.editEmployee = employee;
+      button.setAttribute('data-target', '#updateEmployeeModal');
+    }
+    if (mode === 'delete') {
+      this.deleteEmployee = employee ;
+      button.setAttribute('data-target', '#deleteEmployeeModal');
+    }
+    container?.appendChild(button);
+    button.click();
+  }
+
+
+
+
 
 }
